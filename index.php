114d7ca89a701278fd482484bb04e805769508d6
mygoare <mygoare@gmail.com>
2013-11-13 13:56:04 +0800

    fix some bugs: 1. 修复exif_imagetype 函数当 图片size太小时引发的报错 2. 对所要处理图片进行双重 验证（过滤后缀名 && 验证实际图片类型与后缀相符） 3. add debug function

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ include_once("./lib/resize-class.php");
 //variables config
 $resize_to_width = "190";
 $resize_to_height = "130";
-$srcDir = "/users/mygoare/Pictures/beauty/";
+$srcDir = "/users/mygoare/Pictures/";
 $distDir = dirname(__FILE__)."/dist/";
 if (!is_dir($distDir)) {mkdir($distDir);}
 
@@ -21,7 +21,7 @@ function recursionDir($dir){
           if (is_dir($dir.$file)) {
             //递归
             recursionDir($dir.$file."/");
-          } elseif (exif_imagetype($dir.$file)) {
+          } elseif (filesize($dir.$file) > 11 && checkImageExtension($dir.$file)) {
             //key is 路径, value is filename.
             $files_arr[$dir.$file] = $file;
           }
@@ -30,6 +30,45 @@ function recursionDir($dir){
       closedir($dp);
     }
   }
+}
+
+function checkImageExtension($filePath) {
+  $ex_arr = ['.jpg', '.gif', '.png'];
+  $ex = strtolower(strrchr($filePath, '.'));
+  if ($ex == ".jpeg") {
+    $ex = ".jpg";
+  }
+
+  //后缀名 与 实际相符
+  if ($ex && in_array($ex, $ex_arr) && $ex == recheckImageExtension($filePath)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function recheckImageExtension($filePath) {
+  $extension = "";
+  $image_type_arr = getImageSize($filePath);
+  switch ($image_type_arr["mime"]) {
+    case "image/gif":
+      $extension = ".gif";
+      break;
+    case "image/jpeg":
+      $extension = ".jpg";
+      break;
+    case "image/png":
+      $extension = ".png";
+      break;
+    default:
+      break;
+  }
+  return $extension;
+}
+
+function debug() {
+var_dump ($files_arr);
+return false;
 }
 
 if (!empty($files_arr)) {
