07dbf58ab341fe376878b1abdfca33c80dbeade1
mygoare <mygoare@gmail.com>
2013-11-13 10:35:05 +0800

    可以迭代目标目录中的目录文件夹

--- index.php
+++ index.php
@@ -8,22 +8,36 @@ $srcDir = "/users/mygoare/Pictures/beauty/";
 $distDir = dirname(__FILE__)."/dist/";
 if (!is_dir($distDir)) {mkdir($distDir);}
 
-$files_arr = Array();
+$files_arr = array();
 
-if(is_dir($srcDir)) {
-  if($beauty = opendir($srcDir)) {
-    while ( ($file = readdir($beauty)) !== false ) {
-      if ($file != "." && $file != ".." && exif_imagetype($srcDir.$file)) {
-        array_push($files_arr, $file);
+recursionDir($srcDir);
+function recursionDir($dir){
+  // http://stackoverflow.com/questions/5060465/get-variables-from-the-outside-inside-a-function-in-php
+  global $files_arr;
+  if(is_dir($dir)) {
+    if($dp = opendir($dir)) {
+      while ( ($file = readdir($dp)) !== false ) {
+        if ($file != "." && $file != "..") {
+          if (is_dir($dir.$file)) {
+            //递归
+            recursionDir($dir.$file."/");
+          } elseif (exif_imagetype($dir.$file)) {
+            //key is 路径, value is filename.
+            $files_arr[$dir.$file] = $file;
+          }
+        }
       }
+      closedir($dp);
     }
-    closedir($beauty);
   }
 }
 
-foreach ($files_arr as $value) {
-  $resizeObj = new resize($srcDir.$value);
-  $resizeObj->resizeImage($resize_to_width, $resize_to_height, 'crop');
-  $resizeObj->saveImage($distDir.$value);
+if (!empty($files_arr)) {
+  foreach ($files_arr as $key => $value) {
+    $resizeObj = new resize($key);
+    $resizeObj->resizeImage($resize_to_width, $resize_to_height, 'crop');
+    //save image to distDir
+    $resizeObj->saveImage($distDir.$value);
+  }
 }
 ?>
